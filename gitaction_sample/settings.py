d854df9c30073e32a56e922f138919817a2001cf
AbrahamYu <cpttkrjin@gmail.com>
2022-12-30 19:05:52 +0900

    add allowed_host for aws eb editting var2

--- gitaction_sample/settings.py
+++ gitaction_sample/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = 'django-insecure-55uj!^)2!&qp$=&zl&udagoz%yd(xl6i=&b-qgg*+y&67h&a=i
 DEBUG = True
 
 # ALLOWED_HOSTS = []
+# setting eb urls
 ALLOWED_HOSTS = [
     'http://sample-env.eba-2qf5rknm.ap-northeast-2.elasticbeanstalk.com/']
 
